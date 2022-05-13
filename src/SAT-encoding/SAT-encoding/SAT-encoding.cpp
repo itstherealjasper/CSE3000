ec0080c530bbe8f1f33242621be42a432276208e
Jasper Vermeulen <itstherealjasper@gmail.com>
2022-05-13 15:43:33 +0200

    Added critical path for early, late, start and end times

---
+++ src/SAT-encoding/SAT-encoding/SAT-encoding.cpp
@@ -0,0 +1,258 @@
+﻿#include "SAT-encoding.h"
+
+using namespace std;
+
+struct Task
+{
+	int id;
+	int segment;
+	vector<pair<int, int>> successors;
+	int duration;
+	int resource_type;
+	int resource_requirement;
+
+	int early_start = 0;
+	int early_finish = INT_MAX / 2;
+	int late_start = 0;
+	int late_finish = INT_MAX / 2;
+};
+
+struct Task_Id
+{
+private:
+
+	static Task_Id* instance;
+	Task_Id() {};
+
+	int next_unused_id = 0;
+	vector<int> reusable_ids;
+public:
+	static Task_Id* get_instance() {
+		if (!instance) {
+			instance = new Task_Id;
+		};
+		return instance;
+	}
+
+	int get_unused_id()
+	{
+		if (reusable_ids.size() > 0) {
+			int unused_id = reusable_ids[0];
+			reusable_ids.erase(reusable_ids.begin());
+			return unused_id;
+		}
+		return next_unused_id++;
+	}
+
+	void set_reusable_id(int reusable_id)
+	{
+		if (reusable_id >= next_unused_id) {
+			throw new exception("reusable id is outside of the used id range");
+		}
+		reusable_ids.push_back(reusable_id);
+	}
+};
+
+void parse_input_file(string filename);
+void preempt_tasks();
+void preempt_task(Task task);
+void critical_path();
+void forward_pass(pair<int, int> task_segment_id, int earliest_start);
+void backward_pass(pair<int, int> task_segment_id, int late_finish);
+
+// Global algorithm variables
+const int setup_time = 1;
+
+// Global variables for the specific problem instance used
+const string project_lib_filename = "C:/Projects/cse3000/src/heuristic-algorithm/heuristic-algorithm/j30.sm/j301_1.sm";
+
+Task_Id* Task_Id::instance = 0;
+Task_Id* unique_task_id = unique_task_id->get_instance();
+
+int horizon;
+vector<Task> parsed_tasks;
+vector<int> resource_availabilities;
+vector<Task> preempted_tasks;
+
+int main()
+{
+	parse_input_file(project_lib_filename);
+
+	preempt_tasks();
+
+	critical_path();
+
+	return 0;
+}
+
+void parse_input_file(string filename)
+{
+	string tmp;
+
+	ifstream project_lib(filename);
+
+	if (!project_lib.is_open()) {
+		throw runtime_error("Could not open lib file");
+	};
+
+	int task_count;
+
+	for (int i = 0; i < 5; i++)
+	{
+		getline(project_lib, tmp);
+	};
+
+	getline(project_lib, tmp, ':');
+	project_lib >> task_count;
+
+	getline(project_lib, tmp, ':');
+	project_lib >> horizon;
+
+	for (int i = 0; i < 2; i++)
+	{
+		getline(project_lib, tmp);
+	};
+
+	getline(project_lib, tmp, ':');
+	int resource_count;
+	project_lib >> resource_count;
+	getline(project_lib, tmp);
+
+	for (int i = 0; i < 9; i++)
+	{
+		getline(project_lib, tmp);
+	};
+
+	for (int i = 0; i < task_count; i++)
+	{
+		Task task;
+		task.id = unique_task_id->get_unused_id();
+		task.segment = 1;
+		parsed_tasks.push_back(task);
+
+		int successor_count;
+		project_lib >> tmp >> tmp >> successor_count;
+		vector<pair<int, int>> successors(successor_count);
+		for (int j = 0; j < successor_count; j++)
+		{
+			project_lib >> successors[j].first;
+			successors[j].first--;
+			successors[j].second = 1;
+		};
+		parsed_tasks[i].successors = successors;
+	}
+
+
+	for (int i = 0; i < 5; i++)
+	{
+		getline(project_lib, tmp);
+	};
+
+	for (int i = 0; i < task_count; i++)
+	{
+		project_lib >> tmp >> tmp >> parsed_tasks[i].duration;
+		int max_resource_requirement = -1;
+		for (int j = 0; j < resource_count; j++)
+		{
+			int parsed_resource_requirement;
+			project_lib >> parsed_resource_requirement;
+
+			if (parsed_resource_requirement > max_resource_requirement) {
+				max_resource_requirement = parsed_resource_requirement;
+				parsed_tasks[i].resource_requirement = parsed_resource_requirement;
+				parsed_tasks[i].resource_type = j;
+			}
+		};
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		getline(project_lib, tmp);
+	};
+
+	for (int i = 0; i < resource_count; i++)
+	{
+		int resource_availability;
+		project_lib >> resource_availability;
+		resource_availabilities.push_back(resource_availability);
+	}
+
+	// Close the file
+	project_lib.close();
+}
+
+void preempt_tasks()
+{
+	preempted_tasks.push_back(parsed_tasks.front());
+	for each (Task parsed_task in parsed_tasks)
+	{
+		preempt_task(parsed_task);
+	}
+	preempted_tasks.push_back(parsed_tasks.back());
+}
+
+void preempt_task(Task task)
+{
+	for (int i = 1; i <= task.duration; i++)
+	{
+		for (int j = task.duration + 1 - i; j > 0; j--)
+		{
+			Task task_part = task;
+			task_part.segment = i;
+			task_part.duration = j;
+			if (task_part.segment + task_part.duration != task.segment + task.duration) {
+				task_part.successors = vector<pair<int, int>>{ pair<int, int>(task.id, task_part.segment + task_part.duration) };
+			}
+			preempted_tasks.push_back(task_part);
+		}
+	}
+}
+
+void critical_path()
+{
+	forward_pass(pair<int, int>(0, 1), 1);
+	backward_pass(pair<int, int>(preempted_tasks.back().id, 1), horizon);
+}
+
+void forward_pass(pair<int, int> task_segment_id, int early_start)
+{
+	for (int i = 0; i < preempted_tasks.size(); i++)
+	{
+		if (preempted_tasks[i].id == task_segment_id.first && preempted_tasks[i].segment == task_segment_id.second)
+		{
+			if (early_start > preempted_tasks[i].early_start) {
+				preempted_tasks[i].early_start = early_start;
+				preempted_tasks[i].early_finish = early_start + preempted_tasks[i].duration - 1;
+				for (int j = 0; j < preempted_tasks[i].successors.size(); j++)
+				{
+					forward_pass(preempted_tasks[i].successors[j], preempted_tasks[i].early_finish + 1);
+				}
+			}
+		}
+	}
+}
+
+void backward_pass(pair<int, int> task_segment_id, int late_finish)
+{
+	for (int i = 0; i < preempted_tasks.size(); i++)
+	{
+		if (preempted_tasks[i].id == task_segment_id.first && preempted_tasks[i].segment == task_segment_id.second)
+		{
+			if (late_finish < preempted_tasks[i].late_finish) {
+				preempted_tasks[i].late_finish = late_finish;
+				preempted_tasks[i].late_start = late_finish - preempted_tasks[i].duration + 1;
+
+				for (int j = 0; j < preempted_tasks.size(); j++)
+				{
+					for (int k = 0; k < preempted_tasks[j].successors.size(); k++)
+					{
+						if (preempted_tasks[j].successors[k].first == task_segment_id.first && preempted_tasks[j].successors[k].second == task_segment_id.second)
+						{
+							backward_pass(pair<int, int>(preempted_tasks[j].id, preempted_tasks[j].segment), preempted_tasks[i].late_start - 1);
+						}
+					}
+				}
+			}
+		}
+	}
+}
