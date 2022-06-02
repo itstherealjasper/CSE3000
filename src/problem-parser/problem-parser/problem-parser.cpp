fa7403a3aabefa98dc10d35454555ce3580e249e
Jasper Vermeulen <itstherealjasper@gmail.com>
2022-06-02 16:50:10 +0200

    Fixed optimal/heuristic solutions

--- src/problem-parser/problem-parser/problem-parser.cpp
+++ src/problem-parser/problem-parser/problem-parser.cpp
@@ -11,11 +11,11 @@ struct Task
 	vector<int> resource_requirements;
 };
 
-void write_custom_project_file(string file_path, int horizon, vector<Task> parsed_tasks, vector<int> resource_availabilities);
+void write_custom_project_file(string file_path, int horizon, vector<Task> parsed_tasks, vector<int> resource_availabilities, int optimal_heuristic_solution);
 string extract_filename_without_extention(string& file_path);
-void parse_j30_file(string file_path);
-void parse_DC1_file(string file_path);
-void parse_RG30_file(string file_path, string set_directory);
+void parse_j30_file(string file_path, ifstream& optimal_solution_fs);
+void parse_DC1_file(string file_path, ifstream& optimal_solution_fs);
+void parse_RG30_file(string file_path, string set_directory, ifstream& optimal_solution_fs);
 
 const string input_lib_folder = "C:/Projects/cse3000/src/datasets/";
 const string output_lib_folder = "C:/Projects/cse3000/src/parsed-datasets/";
@@ -29,31 +29,98 @@ const vector<string> RG30_set_directories = { "Set1/", "Set2/", "Set3/","Set4/",
 
 int main(int argc, char* argv[])
 {
-	for (const auto& entry : fs::directory_iterator(input_lib_folder + j30_sub_folder)) {
-		parse_j30_file(entry.path().string());
+	ifstream j30_optimal_solution_fs(input_lib_folder + "J30_BKS.csv");
+	if (!j30_optimal_solution_fs.is_open()) {
+		throw runtime_error("Could not open lib file");
+	};
+	vector<string> j30_filenames_sorted;
+	for (auto& entry : fs::directory_iterator(input_lib_folder + j30_sub_folder)) {
+		j30_filenames_sorted.push_back(entry.path().string());
 	}
-
-	for (const auto& entry : fs::directory_iterator(input_lib_folder + DC1_sub_folder)) {
-		parse_DC1_file(entry.path().string());
+	sort(j30_filenames_sorted.begin(), j30_filenames_sorted.end(), [](const string& lhs, const string& rhs) {return make_pair(lhs.length(), cref(lhs)) < make_pair(rhs.length(), cref(rhs)); });
+	//for (int i = 0; i < j30_filenames_sorted.size(); i++)
+	//{
+	//	if (j30_filenames_sorted[i].substr(j30_filenames_sorted[i].size() - 4) == "1.sm") {
+	//		rename(j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size()).c_str(), (j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size() - 4) + "0.sm").c_str());
+	//	}
+	//	else if (j30_filenames_sorted[i].substr(j30_filenames_sorted[i].size() - 4) == "2.sm") {
+	//		rename(j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size()).c_str(), (j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size() - 4) + "1.sm").c_str());
+	//	}
+	//	else if (j30_filenames_sorted[i].substr(j30_filenames_sorted[i].size() - 4) == "3.sm") {
+	//		rename(j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size()).c_str(), (j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size() - 4) + "2.sm").c_str());
+	//	}
+	//	else if (j30_filenames_sorted[i].substr(j30_filenames_sorted[i].size() - 4) == "4.sm") {
+	//		rename(j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size()).c_str(), (j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size() - 4) + "3.sm").c_str());
+	//	}
+	//	else if (j30_filenames_sorted[i].substr(j30_filenames_sorted[i].size() - 4) == "5.sm") {
+	//		rename(j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size()).c_str(), (j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size() - 4) + "4.sm").c_str());
+	//	}
+	//	else if (j30_filenames_sorted[i].substr(j30_filenames_sorted[i].size() - 4) == "6.sm") {
+	//		rename(j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size()).c_str(), (j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size() - 4) + "5.sm").c_str());
+	//	}
+	//	else if (j30_filenames_sorted[i].substr(j30_filenames_sorted[i].size() - 4) == "7.sm") {
+	//		rename(j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size()).c_str(), (j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size() - 4) + "6.sm").c_str());
+	//	}
+	//	else if (j30_filenames_sorted[i].substr(j30_filenames_sorted[i].size() - 4) == "8.sm") {
+	//		rename(j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size()).c_str(), (j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size() - 4) + "7.sm").c_str());
+	//	}
+	//	else if (j30_filenames_sorted[i].substr(j30_filenames_sorted[i].size() - 4) == "9.sm") {
+	//		rename(j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size()).c_str(), (j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size() - 4) + "8.sm").c_str());
+	//	}
+	//	else if (j30_filenames_sorted[i].substr(j30_filenames_sorted[i].size() - 5) == "10.sm") {
+	//		rename(j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size()).c_str(), (j30_filenames_sorted[i].substr(0, j30_filenames_sorted[i].size() - 5) + "9.sm").c_str());
+	//	}
+	//	else {
+	//		continue;
+	//	}
+	//}
+	for (auto& filename : j30_filenames_sorted) {
+		parse_j30_file(filename, j30_optimal_solution_fs);
 	}
+	j30_optimal_solution_fs.close();
 
+	ifstream DC1_optimal_solution_fs(input_lib_folder + "DC1_BKS.csv");
+	if (!DC1_optimal_solution_fs.is_open()) {
+		throw runtime_error("Could not open lib file");
+	};
+	vector<string> DC1_filenames_sorted;
+	for (auto& entry : fs::directory_iterator(input_lib_folder + DC1_sub_folder)) {
+		DC1_filenames_sorted.push_back(entry.path().string());
+	}
+	sort(DC1_filenames_sorted.begin(), DC1_filenames_sorted.end(), [](const string& lhs, const string& rhs) {return make_pair(lhs.length(), cref(lhs)) < make_pair(rhs.length(), cref(rhs)); });
+	for (auto& filename : DC1_filenames_sorted) {
+		parse_DC1_file(filename, DC1_optimal_solution_fs);
+	}
+	DC1_optimal_solution_fs.close();
+
+	ifstream RG30_optimal_solution_fs(input_lib_folder + "RG30_BKS.csv");
+	if (!RG30_optimal_solution_fs.is_open()) {
+		throw runtime_error("Could not open lib file");
+	};
 	for (int i = 0; i < RG30_set_directories.size(); i++)
 	{
-		for (const auto& entry : fs::directory_iterator(input_lib_folder + RG30_sub_folder + RG30_set_directories[i])) {
-			parse_RG30_file(entry.path().string(), RG30_set_directories[i]);
+		vector<string> RG30_filenames_sorted;
+		for (auto& entry : fs::directory_iterator(input_lib_folder + RG30_sub_folder + RG30_set_directories[i])) {
+			RG30_filenames_sorted.push_back(entry.path().string());
+		}
+		sort(RG30_filenames_sorted.begin(), RG30_filenames_sorted.end(), [](const string& lhs, const string& rhs) {return make_pair(lhs.length(), cref(lhs)) < make_pair(rhs.length(), cref(rhs)); });
+		for (auto& filename : RG30_filenames_sorted) {
+			parse_RG30_file(filename, RG30_set_directories[i], RG30_optimal_solution_fs);
 		}
 	}
+	RG30_optimal_solution_fs.close();
 
 	return 0;
 }
 
-void write_custom_project_file(string file_path, int horizon, vector<Task> parsed_tasks, vector<int> resource_availabilities)
+void write_custom_project_file(string file_path, int horizon, vector<Task> parsed_tasks, vector<int> resource_availabilities, int optimal_heuristic_solution)
 {
 	stringstream lib_file_content;
 
 	lib_file_content << "h;" << horizon << '\n';
 	lib_file_content << "s;" << parsed_tasks.size() << '\n';
 	lib_file_content << "r;" << resource_availabilities.size() << '\n';
+	lib_file_content << "o;" << optimal_heuristic_solution << '\n';
 	lib_file_content << '\n';
 	lib_file_content << resource_availabilities[0];
 	for (int i = 1; i < resource_availabilities.size(); i++)
@@ -88,7 +155,7 @@ void write_custom_project_file(string file_path, int horizon, vector<Task> parse
 	}
 }
 
-void parse_j30_file(string file_path)
+void parse_j30_file(string file_path, ifstream& optimal_solution_fs)
 {
 	string tmp;
 
@@ -103,6 +170,12 @@ void parse_j30_file(string file_path)
 	int resource_count;
 	vector<Task> parsed_tasks;
 	vector<int> resource_availabilities;
+	int optimal_heuristic_solution;
+
+	getline(optimal_solution_fs, tmp, ';');
+	getline(optimal_solution_fs, tmp, ';');
+	optimal_solution_fs >> optimal_heuristic_solution;
+	getline(optimal_solution_fs, tmp);
 
 	for (int i = 0; i < 5; i++)
 	{
@@ -177,7 +250,7 @@ void parse_j30_file(string file_path)
 	project_lib.close();
 
 	string file_without_extension = extract_filename_without_extention(file_path);
-	write_custom_project_file(output_lib_folder + j30_sub_folder + file_without_extension, horizon, parsed_tasks, resource_availabilities);
+	write_custom_project_file(output_lib_folder + j30_sub_folder + file_without_extension, horizon, parsed_tasks, resource_availabilities, optimal_heuristic_solution);
 }
 
 string extract_filename_without_extention(string& file_path)
@@ -188,8 +261,10 @@ string extract_filename_without_extention(string& file_path)
 	return base_filename.substr(0, p);
 }
 
-void parse_DC1_file(string file_path)
+void parse_DC1_file(string file_path, ifstream& optimal_solution_fs)
 {
+	string tmp;
+
 	ifstream project_lib(file_path);
 
 	if (!project_lib.is_open()) {
@@ -201,6 +276,12 @@ void parse_DC1_file(string file_path)
 	int resource_count;
 	vector<Task> parsed_tasks;
 	vector<int> resource_availabilities;
+	int optimal_heuristic_solution;
+
+	getline(optimal_solution_fs, tmp, ';');
+	getline(optimal_solution_fs, tmp, ';');
+	optimal_solution_fs >> optimal_heuristic_solution;
+	getline(optimal_solution_fs, tmp);
 
 	project_lib >> task_count >> resource_count;
 	resource_availabilities = vector<int>(resource_count);
@@ -238,11 +319,13 @@ void parse_DC1_file(string file_path)
 	project_lib.close();
 
 	string file_without_extension = extract_filename_without_extention(file_path);
-	write_custom_project_file(output_lib_folder + DC1_sub_folder + file_without_extension, horizon, parsed_tasks, resource_availabilities);
+	write_custom_project_file(output_lib_folder + DC1_sub_folder + file_without_extension, horizon, parsed_tasks, resource_availabilities, optimal_heuristic_solution);
 }
 
-void parse_RG30_file(string file_path, string set_directory)
+void parse_RG30_file(string file_path, string set_directory, ifstream& optimal_solution_fs)
 {
+	string tmp;
+
 	ifstream project_lib(file_path);
 
 	if (!project_lib.is_open()) {
@@ -254,6 +337,12 @@ void parse_RG30_file(string file_path, string set_directory)
 	int resource_count;
 	vector<Task> parsed_tasks;
 	vector<int> resource_availabilities;
+	int optimal_heuristic_solution;
+
+	getline(optimal_solution_fs, tmp, ';');
+	getline(optimal_solution_fs, tmp, ';');
+	optimal_solution_fs >> optimal_heuristic_solution;
+	getline(optimal_solution_fs, tmp);
 
 	project_lib >> task_count >> resource_count;
 	resource_availabilities = vector<int>(resource_count);
@@ -291,5 +380,5 @@ void parse_RG30_file(string file_path, string set_directory)
 	project_lib.close();
 
 	string file_without_extension = extract_filename_without_extention(file_path);
-	write_custom_project_file(output_lib_folder + RG30_sub_folder + set_directory + file_without_extension, horizon, parsed_tasks, resource_availabilities);
+	write_custom_project_file(output_lib_folder + RG30_sub_folder + set_directory + file_without_extension, horizon, parsed_tasks, resource_availabilities, optimal_heuristic_solution);
 }
